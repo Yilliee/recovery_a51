8ce16cde8b93b04826ab28d5f26cfccbf37dc077
Ian Macdonald <ian@caliban.org>
2020-10-20 10:52:03 +0200

    Use PRODUCT_USE_DYNAMIC_PARTITIONS to set ro.boot.dynamic_partitions.

--- omni_a51.mk
+++ omni_a51.mk
@@ -1,8 +1,8 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Include any options that can't be included in BoardConfig.mk
+$(call inherit-product, device/samsung/a51/device.mk)
 
 # Inherit from those products. Most specific first.
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
@@ -10,5 +10,6 @@ $(call inherit-product, vendor/omni/config/common.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_a51
 PRODUCT_DEVICE := a51
+PRODUCT_MODEL := SM-A515F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
