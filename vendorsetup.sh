0a4a11cb8b2fd86fc60f5cb7b09a50099db9f097
Yilliee <m.taha6f@gmail.com>
2021-02-12 13:12:35 +0000

    Adding necessary Build Vars

---
+++ vendorsetup.sh
@@ -0,0 +1,43 @@
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export LC_ALL="C"
+export OF_VANILLA_BUILD=1
+export OF_NO_SAMSUNG_SPECIAL=0
+export FOX_DYNAMIC_SAMSUNG_FIX=1
+#export FOX_REMOVE_AAPT=1
+#export FOX_REMOVE_BASH=1
+export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/13520000.ufs/by-name/recovery"
+
+# Extra build vars
+#export FOX_REPLACE_BUSYBOX_PS=1
+#export FOX_REPLACE_TOOLBOX_GETPROP=1
+#export FOX_USE_TAR_BINARY=1
+#export FOX_USE_NANO_EDITOR=1
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+#export FOX_RESET_SETTINGS=0
+#export FOX_DELETE_AROMAFM=1
+#export OF_FL_PATH1="/sys/devices/virtual/camera/flash/rear_flash"
+#export OF_FL_PATH2="/sys/devices/virtual/camera/flash/rear_flash"
+export OF_FLASHLIGHT_ENABLE=0
+export FOX_VERSION="R11.0"
+export OF_SCREEN_H="2400"
+export OF_HIDE_NOTCH=1
+export OF_CLOCK_POS=1
+export OF_ALLOW_DISABLE_NAVBAR=0
+export TARGET_DEVICE_ALT="a51, a51ns, a51nsxx"
+export OF_TARGET_DEVICES="a51, a51ns, a51nsxx"
+#export OF_USE_SYSTEM_FINGERPRINT=1
+export OF_USE_TWRP_SAR_DETECT=1
+export OF_QUICK_BACKUP_LIST="/super;/boot;/prism_image;/optics_image"
+#export FOX_USE_UNZIP_BINARY=1
+export OF_DEVICE_WITHOUT_PERSIST=1
+export FOX_DISABLE_APP_MANAGER=1
+#export FOX_USE_XZ_UTILS=1
+export OF_STATUS_INDENT_LEFT="48"
+export OF_STATUS_INDENT_RIGHT="48"
+export OF_STATUS_H="98"
+
+#R11
+export FOX_R11=1
+export FOX_BUILD_TYPE=Beta
