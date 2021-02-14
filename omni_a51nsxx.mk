5422843eb3d91c697e0e4c91c67a31f3dc6c4381
Yillie <m.taha6f@gmail.com>
2021-02-14 13:20:35 +0500

    Include device.mk

--- omni_a51nsxx.mk
+++ omni_a51nsxx.mk
@@ -1,6 +1,9 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
+# Include any options that can't be included in BoardConfig.mk
+$(call inherit-product, device/samsung/a51/device.mk)
+
 # Inherit from those products. Most specific first.
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
 
