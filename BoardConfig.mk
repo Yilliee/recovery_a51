656f87d3cd0ea9ec4381021c07bd2d7e602741d2
Yillié <yilliee@protonmail.com>
2022-07-10 19:49:55 +0500

    BoardConfig : Update defconfig name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@ DEVICE_PATH := device/samsung/a51
 # Kernel
 #BUILD_KERNEL_FROM_SOURCE := true
 ifeq ($(BUILD_KERNEL_FROM_SOURCE), true)
-	TARGET_KERNEL_CONFIG := celicia-rec-a51_defconfig
+	TARGET_KERNEL_CONFIG := celicia-a51_defconfig
 	BOARD_DTBO_CFG := $(DEVICE_PATH)/kernel/a51.cfg
 else
 	TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/zImage
